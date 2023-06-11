c3ce9f16e361dee506d3b8c319a054279f414455
Eric Vargas <eric.vargas.mx@gmail.com>
2023-06-10 20:03:26 -0500

    Toggle between camera and photo

--- app/components/camera.tsx
+++ app/components/camera.tsx
@@ -12,6 +12,7 @@ const videoConstraints = {
 
 const Camera = () => {
   const [imgSrc, setImgSrc] = useState<string | null>(null);
+  const [showCam, setShowCam] = useState<boolean>(false);
   const webcamRef = React.useRef<Webcam>(null);
   const capture = React.useCallback(() => {
     const imageSrc = webcamRef.current?.getScreenshot();
@@ -19,25 +20,39 @@ const Camera = () => {
       return;
     }
     setImgSrc(imageSrc);
+    setShowCam(false);
   }, [webcamRef]);
   return (
     <div className="flex flex-col gap-2">
-      <Webcam
-        audio={false}
-        ref={webcamRef}
-        screenshotFormat="image/png"
-        width={1280}
-        height={720}
-        videoConstraints={videoConstraints}
-      />
-      <button
-        onClick={capture}
-        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >
-        Tomar foto
-      </button>
-      {imgSrc && (
-        <Image src={imgSrc} width={1280} height={720} alt="my image" />
+      {!showCam ? (
+        <>
+          {imgSrc && (
+            <Image src={imgSrc} width={1280} height={720} alt="my image" />
+          )}
+          <button
+            onClick={() => setShowCam(true)}
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Usar camara
+          </button>
+        </>
+      ) : (
+        <>
+          <Webcam
+            audio={false}
+            ref={webcamRef}
+            screenshotFormat="image/png"
+            width={1280}
+            height={720}
+            videoConstraints={videoConstraints}
+          />
+          <button
+            onClick={capture}
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Tomar foto
+          </button>
+        </>
       )}
     </div>
   );
