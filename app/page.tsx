68c987e4c328befdb3416fd38fb36b7f79f8fd15
Eric Vargas <eric.vargas.mx@gmail.com>
2023-06-15 18:44:21 -0500

    Add name

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,23 @@ export default function Home() {
       <div className="flex flex-col w-80 justify-center items-center">
         <div className="w-full p-6 bg-white border border-gray-200 rounded-lg shadow">
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" action="#" method="POST">
+            <div className="space-y-6">
+              <div>
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Nombre
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="username"
+                    name="username"
+                    type="text"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
               <div>
                 <label
                   htmlFor="email"
@@ -27,22 +43,20 @@ export default function Home() {
                     name="email"
                     type="email"
                     autoComplete="email"
-                    required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
 
               <div>
-                {/* <button
-                  type="submit"
+                <Link
+                  href="/step2"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Continuar
-                </button> */}
-                <Link href="/step2" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Continuar</Link>
+                </Link>
               </div>
-            </form>
+            </div>
           </div>
         </div>
       </div>
