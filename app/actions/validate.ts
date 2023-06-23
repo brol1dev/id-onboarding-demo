8f4589977b28d7013415dda56eb6dff531e5a160
Eric Vargas <eric.vargas.mx@gmail.com>
2023-06-23 13:42:50 -0500

    Test process.env.VERCEL_URL

--- app/actions/validate.ts
+++ app/actions/validate.ts
@@ -7,8 +7,9 @@ import { redirect } from "next/navigation";
 export default async function validate(store: StoreType) {
   if (process.env.NODE_ENV === "development") {
     setTimeout(2000);
-  } else if (process.env.NODE_ENV === "production") {
-    const res = await fetch("/api/python");
+  } else if (process.env.VERCEL_URL)  {
+    console.log("Vercel");
+    const res = await fetch(`https://${process.env.VERCEL_URL}/api/python`);
     console.log(res);
     const json = await res.json();
     console.log(json);
