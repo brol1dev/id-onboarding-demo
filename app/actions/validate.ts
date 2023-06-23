d2b928c1d251c0b683bb646e9c4436e219a1eed8
Eric Vargas <eric.vargas.mx@gmail.com>
2023-06-23 13:24:49 -0500

    Fix validate action

--- app/actions/validate.ts
+++ app/actions/validate.ts
@@ -5,19 +5,6 @@ import { StoreType } from "../hooks/useStore";
 import { redirect } from "next/navigation";
 
 export default async function validate(store: StoreType) {
-  const res = await fetch("http://localhost:8080/users", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ test: "test" }),
-  });
-
-  if (!res.ok) {
-    console.log("failure");
-  } else {
-    console.log("success");
-  }
-
+  setTimeout(2000);
   redirect("/valid");
 }
