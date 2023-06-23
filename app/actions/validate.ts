e9628a0883fdc9fef12ad64c24d649109ef196c1
Eric Vargas <eric.vargas.mx@gmail.com>
2023-06-23 13:38:43 -0500

    Testing validate function for prod

--- app/actions/validate.ts
+++ app/actions/validate.ts
@@ -5,6 +5,19 @@ import { StoreType } from "../hooks/useStore";
 import { redirect } from "next/navigation";
 
 export default async function validate(store: StoreType) {
-  setTimeout(2000);
+  if (process.env.NODE_ENV === "development") {
+    setTimeout(2000);
+  } else if (process.env.NODE_ENV === "production") {
+    const res = await fetch("/api/python");
+    console.log(res);
+    const json = await res.json();
+    console.log(json);
+  }
+
+  
+  
+  
+  // const json = await res.json();
+  // console.log(json);
   redirect("/valid");
 }
