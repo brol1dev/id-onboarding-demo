129b8c549266231c560f277b3e7ee986f67880c0
Eric Vargas <eric.vargas.mx@gmail.com>
2023-06-24 17:18:51 -0500

    Comment deepface dependency

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from deepface import DeepFace
+# from deepface import DeepFace
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -9,17 +9,17 @@ def validate():
     request_json = request.get_json()
     print("Received new request with json body")
 
-    print("About to compare images")
+    # print("About to compare images")
     # Deepface accepts b64 encoded images
-    result = DeepFace.verify(request_json["frontImg"], request_json["photoImg"])
+    # result = DeepFace.verify(request_json["frontImg"], request_json["photoImg"])
 
-    if "verified" not in result:
-        print("verified key not present in Deepface result")
-        return jsonify({"valid": False})
+    # if "verified" not in result:
+    #     print("verified key not present in Deepface result")
+    #     return jsonify({"valid": False})
 
-    verified = result["verified"]
-    print("Compared images successfully: ", verified)
+    # verified = result["verified"]
+    # print("Compared images successfully: ", verified)
 
-    if verified:
-        return jsonify({"valid": True})
-    return jsonify({"valid": False})
+    # if verified:
+    #     return jsonify({"valid": True})
+    return jsonify({"valid": True})
