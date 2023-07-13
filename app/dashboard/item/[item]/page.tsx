2c5f6cd3e4254ae9853703504fdae3928721c686
Eric Vargas <eric.vargas.mx@gmail.com>
2023-07-12 21:47:46 -0500

    Add lower fixed panel

--- app/dashboard/item/[item]/page.tsx
+++ app/dashboard/item/[item]/page.tsx
@@ -11,5 +11,5 @@ type Props = {
 export default function Item({ params }: Props) {
   const store = useStore();
   console.log(store);
-  return <>{params.item}</>;
+  return <><div className="flex fixed bottom-0 left-0 right-0 bg-gray-300 p-4"><button>Aprobar</button><button>Denegar</button></div></>;
 }
